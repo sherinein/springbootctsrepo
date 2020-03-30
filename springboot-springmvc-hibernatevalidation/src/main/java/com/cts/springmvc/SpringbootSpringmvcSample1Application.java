de4f878033c75eaa46ca702577d7deb6fe8f2099
sherinein <sherinein@gmail.com>
2020-03-30 22:10:52 +0530

    "initial commit"

---
+++ springboot-springmvc-hibernatevalidation/src/main/java/com/cts/springmvc/SpringbootSpringmvcSample1Application.java
@@ -0,0 +1,13 @@
+package com.cts.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringmvcSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringmvcSample1Application.class, args);
+	}
+
+}
